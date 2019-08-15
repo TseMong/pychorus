7b197f816a9bf84ebeaccbfb61ceda0480970c21
mz <highcxy196@outlook.com>
2019-08-15 16:58:25 +0800

    add test for 200 songs

---
+++ test.py
@@ -0,0 +1,25 @@
+import chorus.get_chorus as get_chorus
+import os
+import numpy as np
+import pandas as pd
+
+origin_root = 'D:/Original'
+accompany_root = 'D:/Accompony'
+lyric_root = 'D:/Lyrics_utf8'
+result_root = './data/chorus_audio'
+songListFile = './data/table/final.csv'
+
+songList = pd.read_csv(songListFile)
+
+if __name__ == '__main__':
+    #names = songList.loc[:199, 'songname']
+    ids = songList.loc[:199, 'songID']
+
+    songPath, accPath, lyricPath = [], [], []
+    for idx in ids:
+        songPath.append(origin_root + '/' + str(idx) + '.mp3')
+        accPath.append(accompany_root + '/' + str(idx) + '.mp3')
+        lyricPath.append(lyric_root + '/' + str(idx) + '.txt')
+    print(songPath[:3], accPath[:3], lyricPath[:3])
+    #np.save('./data/chorus_audio/{}.npy'.format('200songs_highlights'), get_chorus.extract(songPath, accPath, lyricPath, save_wav=True))
+    
