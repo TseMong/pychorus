5890e0c2bb3d56f5f7d910a30ef74d9d5f4f635e
mz <highcxy196@outlook.com>
2019-09-02 13:57:59 +0800

    add codecs

--- main.py
+++ main.py
@@ -10,24 +10,24 @@ accs_root = os.path.join(root, 'accompany')
 lyric_root = os.path.join(root, 'lyrics')
 json_root = os.path.join(root, 'json')
 
-class paragraph_parser():
-    def __init__(self, lyric_info, seg_point):
-        self.sentence = lyric_info[:, -1]   #   句子文本
-        self.start = lyric_info[:, 0].astype(np.float)  #   每句开始
-        self.end = lyric_info[:, 1].astypr(np.float)    #   每句结束
-        self.duration = lyric_info[:, 2].astype(np.float)   #   当前句与上一句间隔
-        self.seg_candidate = seg_point  #   msaf的分割点
+# class paragraph_parser():
+#     def __init__(self, lyric_info, seg_point):
+#         self.sentence = lyric_info[:, -1]   #   句子文本
+#         self.start = lyric_info[:, 0].astype(np.float)  #   每句开始
+#         self.end = lyric_info[:, 1].astypr(np.float)    #   每句结束
+#         self.duration = lyric_info[:, 2].astype(np.float)   #   当前句与上一句间隔
+#         self.seg_candidate = seg_point  #   msaf的分割点
     
-    def get_intro(self):    #   得到前奏
-        vocal_start = self.start[0]
-        intro = []
-        for idx in range(1, len(self.seg_candidate)):   #   msaf第一个断点为0
-            if abs(self.seg_candidate[idx] - vocal_start) >= 5:
-                intro.append([seg_candidate[idx-1], self.seg_candidate[idx]])
-            else:
-                intro.append([seg_candidate[idx-1], round(vocal_start, 3)])
-        self.intro = intro
-        return intro
+#     def get_intro(self):    #   得到前奏
+#         vocal_start = self.start[0]
+#         intro = []
+#         for idx in range(1, len(self.seg_candidate)):   #   msaf第一个断点为0
+#             if abs(self.seg_candidate[idx] - vocal_start) >= 5:
+#                 intro.append([seg_candidate[idx-1], self.seg_candidate[idx]])
+#             else:
+#                 intro.append([seg_candidate[idx-1], round(vocal_start, 3)])
+#         self.intro = intro
+#         return intro
 
 
 
@@ -44,16 +44,16 @@ if __name__ == '__main__':
         lyricPath.append(lyric_root + '/' + name + '.txt')
     np.save('./data/chorus_audio/{}.npy'.format('30songs_highlights'), get_chorus.extract(songPath, accPath, lyricPath, save_wav=False))
     '''
-    with open('30songs_seg.json', 'r') as f:
+    with codecs.open('30songs_seg.json', mode='r', encoding='GBK') as f:
         seg = json.load(f)
     #print(seg)
-    with open('highlight.json', 'r') as f:
+    with codecs.open('highlight.json', mode='r', encoding='GBK') as f:
         point = json.load(f)
-    #print(point)
-    lyric_info = get_lyric_seg(os.path.join(lyric_root, '不再犹豫' + '.txt'))
+    # #print(point)
+    lyric_info = get_lyric_seg(os.path.join(lyric_root, '因为爱情' + '.txt'))
     for idx in range(lyric_info.shape[0]):
         print('{}\t{}\t{}\t->{}\t{}'.format(idx, round(float(lyric_info[idx][-2]), 3), round(float(lyric_info[idx][0]), 3), round(float(lyric_info[idx][1]), 3), lyric_info[idx][-1]))
-    print(point['不再犹豫'], seg['不再犹豫'][np.argmin(abs(np.array(seg['不再犹豫']) - point['不再犹豫']))])
-    print(seg['不再犹豫'])
+    print(point['因为爱情'], seg['因为爱情'][np.argmin(abs(np.array(seg['因为爱情']) - point['因为爱情']))])
+    print(seg['因为爱情'])
 
     #main()
